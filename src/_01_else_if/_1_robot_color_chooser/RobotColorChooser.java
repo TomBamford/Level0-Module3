33fbbe241ca8229bb4fc12326f9e144876e05ae3
root <root@5dec17be5f65>
2020-08-08 20:32:02 +0000

    Working on robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,12 +2,37 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+		Robot rob = new Robot("mini");
+		rob.setPenColor(0, 0, 0);
+		rob.penDown();
+		rob.setPenWidth(10);
+		rob.setSpeed(100);
+		for(int j = 0; j < 4; j++) {
+			String input = JOptionPane.showInputDialog("What color would you like the robot to draw with?");
+
+			if(input.equalsIgnoreCase("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(input.equalsIgnoreCase("blue")) {
+				rob.setPenColor(0,0,255);
+			} else if(input.equalsIgnoreCase("green")) {
+				rob.setPenColor(0,255,0);
+			} else {
+				rob.setRandomPenColor();
+			}
+			for(int i=1; i<5; i++) {
+				rob.move(50);
+				rob.turn(90);
+			}
+		
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
@@ -17,7 +42,7 @@ public class RobotColorChooser {
 		
 
 		
-
+		}
 
 
 	}
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,10 +3,17 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String input = JOptionPane.showInputDialog("How many cats do you have?");
+		int crazy = Integer.parseInt(input);
+		if(crazy>3) {
+			
+		}
 		// 2. Convert their answer into an int
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
